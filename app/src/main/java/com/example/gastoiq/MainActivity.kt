b925109e338075f370814b4eb8ff07319da6d3c6
Bryan Taco <btaco3565@gmail.com>
2026-03-08 18:48:02 -0500

    Revert "Merge pull request #1 from themis65/feature/ui-crud"
    This reverts commit a842870dcb7fbc8b8472a43cc7fe3acad2df5e7e, reversing
    changes made to 807feb673e8e31cb39bc8b1a38e49255210c992a.

--- app/src/main/java/com/example/gastoiq/MainActivity.kt
+++ app/src/main/java/com/example/gastoiq/MainActivity.kt
@@ -1,33 +1,20 @@
 package com.example.gastoiq
 
-import android.content.Intent
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.gastoiq.databinding.ActivityMainBinding
-import com.example.gastoiq.ui.AnalyticsActivity
-import com.example.gastoiq.ui.ExpenseFormActivity
-import com.example.gastoiq.ui.HistoryActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.fabAdd.setOnClickListener {
-            startActivity(Intent(this, ExpenseFormActivity::class.java))
-        }
-
-        binding.btnHistory.setOnClickListener {
-            startActivity(Intent(this, HistoryActivity::class.java))
-        }
-
-        binding.btnAnalytics.setOnClickListener {
-            startActivity(Intent(this, AnalyticsActivity::class.java))
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
